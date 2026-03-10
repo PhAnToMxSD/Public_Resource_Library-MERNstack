cc22ed0e99095143c76c803c4f29e2488fe1ea03
Aradhya Agrawal <aradhya.gzb08@gmail.com>
2026-03-10 17:00:44 +0530

    setup for tailwind css done for inclusion, now styling can be done seemlessly

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,17 +1,30 @@
 import { useState } from "react";
-import { auth } from "../config/firebase.js";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, provider } from "../config/firebase.js";
+import { createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+
 
 export const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const signUp = async () => {
-    await createUserWithEmailAndPassword(auth, email, password);
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <div>
+    <div className = "mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
       <input
         type="email"
         placeholder="Email"
@@ -23,6 +36,7 @@ export const Auth = () => {
         onChange={(e) => setPassword(e.target.value)}
       />
       <button onClick={signUp}>Submit</button>
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
     </div>
   );
 };
