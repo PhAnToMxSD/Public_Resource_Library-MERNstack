ff2f0a33ec005ebf2205a58ed45671b487ed1f81
Aradhya Agrawal <aradhya.gzb08@gmail.com>
2026-03-12 02:33:03 +0530

    added the styles to the login box, removed the login with email and password and thus now I am left only with the google login, which is fine because this is a developement phase project.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,28 @@ import { Auth } from "./components/Auth";
 import { ResourceRender } from "./components/ResourceRender";
 
 function App() {
+  const styler = {
+  color: "#2CD367",
+  filter: "drop-shadow(0px 0px 8px green)",
+  fontFamily: '"Datatype", monospace',
+  fontSize: "40px",
+  fontWeight: 400,
+  fontStyle: "normal"
+  }
+
+  const stylere = {
+  color: "#2CD367",
+  filter: "drop-shadow(0px 0px 8px green)",
+  fontFamily: '"Datatype", monospace',
+  fontWeight: 400,
+  fontSize: "90px",
+  fontStyle: "normal"
+  }
+
   return (
-    <div >
-      <h1>GasStation - BY PhAnToMxSD</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 style = {stylere}>GasStation </h1>
+      <h2 style = {styler}>A Public Resource Library - by PhAnToMxSD</h2>
       <Auth />
       <ResourceRender />
     </div>
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,24 +1,17 @@
 import { useState } from "react";
 import { auth, provider } from "../config/firebase.js";
-import { createUserWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
-
-
+import {
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 
 export const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const signUp = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
   const signInWithGoogle = async () => {
     try {
       await signInWithPopup(auth, provider);
+      setIsSignedIn(true);
     } catch (error) {
       console.log(error);
     }
@@ -27,36 +20,16 @@ export const Auth = () => {
   const logout = async () => {
     try {
       await signOut(auth);
+      setIsSignedIn(false);
     } catch (err) {
       console.error(err);
     }
   };
 
-  const buttonColor = "bg-sky-500 hover:bg-sky-700 rounded-lg";
-
   return (
-    <div>
-      <input
-        type="email"
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button className={buttonColor} onClick={signUp}>
-        Click Me
-      </button>
-      <button id = "googleSignIn"
-        className="bg-sky-500 hover:bg-sky-700 rounded-lg hover:border-sky-700"
-        onClick={() => {signInWithGoogle(); document.getElementById("googleSignIn").innerHTML="Signed In"}}
-      >
-        Sign in with Google
-      </button>
-      <button className={buttonColor} onClick={() => {logout(); document.getElementById("googleSignIn").innerHTML="Sign in with Google"}}>
-        Logout
+    <div className="flex flex-col justify-center items-center gap-4">
+      <button className={isSignedIn ? "inputauthD" : "inputauthC"} onClick={isSignedIn ? logout : signInWithGoogle}>
+        {isSignedIn ? "Logout" : "Sign in with Google"}
       </button>
     </div>
   );
