5a53e3392a7609eee81a486c04ecaf606a4f4704
Aradhya Agrawal <aradhya.gzb08@gmail.com>
2026-03-11 12:18:29 +0530

    Created a new folder named backend for express-based developement

---
+++ BACKEND/routes/notesRoutes.js
@@ -0,0 +1,15 @@
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.put('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.post('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.delete('/', (req, res) => {
+  res.send('Hello, World!');
+});
